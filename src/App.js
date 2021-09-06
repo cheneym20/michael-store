115bd801b29ed8e13f42027007ab4f2f2bf4bf4e
Saia Fonua <saiafonua@gmail.com>
2021-09-06 17:15:47 -0600

    use Array.isArray to determine data type

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import './App.css';
 
 
 function App() {
-
-const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/1`)
+    fetch(`https://fakestoreapi.com/products/2`)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         setProducts(data);
       })
   }, [])
@@ -27,13 +25,22 @@ const [products, setProducts] = useState([]);
         <p>You can Do THIS!!!!!!y</p>
       </header>
 
-      {products?.map((product) => (
-        <div className="event" key={product.id}>
-          <li>
-            <b>Name</b>: {product.title}
-          </li>
-        </div>
-      ))}
+      {
+        Array.isArray(products) ?
+          products?.map((product) => (
+            <div className="event" key={product.id}>
+              <li>
+                <b>Name</b>: {product.title}
+              </li>
+            </div>
+          ))
+          : <div className="event" key={products.id}>
+            <li>
+              <b>Name</b>: {products.title}
+            </li>
+          </div>
+
+      }
 
     </div>
   );
